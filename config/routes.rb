f1bb4d4e18a82913bf61d8c2e7e0f806e98882e9
Will Fisher <will@teknofire.net>
2012-02-28 16:58:43 -0900

    [#25675801 #25675391] updating initial page view and headers

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ Iceobs::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  root :to => 'observations#index'
 
   # See how all your routes lay out with "rake routes"
 
