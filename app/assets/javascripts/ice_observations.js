25ade7c88845b17f272864e6644e557a1ab74f87
Scott Macfarlane <scott@gina.alaska.edu>
2012-03-14 10:13:58 -0800

    Update form layous

--- app/assets/javascripts/ice_observations.js
+++ app/assets/javascripts/ice_observations.js
@@ -1,3 +1,11 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+$(document).ready(function() {
+
+  $("#ice_total_concentration").blur( function(){
+    if( $(this).val() === "" ) {
+      $(this).val(0);
+    }
+    $("#tc.infotext").html("Total Concentration: " + $(this).val() + "%");
+  });
+
+
+});
