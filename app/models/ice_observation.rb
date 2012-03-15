1a034c0615e55a855e91a748e7906fce2fd695a8
Scott Macfarlane <scott@gina.alaska.edu>
2012-03-15 12:44:26 -0800

    Add dummy help buttons to all fields

--- app/models/ice_observation.rb
+++ app/models/ice_observation.rb
@@ -2,6 +2,7 @@ class IceObservation < ActiveRecord::Base
   belongs_to :observation
   has_one :melt_pond
   has_one :topography
+  belongs_to :ice_lookup
 
   accepts_nested_attributes_for :melt_pond
   accepts_nested_attributes_for :topography
