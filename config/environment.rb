0f1735958bcf3df604129bfbf1e61b7cdc65df85
Scott Macfarlane <scott@gina.alaska.edu>
2012-03-30 14:43:02 -0800

    Let java set database location

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,6 @@
 require File.expand_path('../application', __FILE__)
 
 if RUBY_PLATFORM == "java"
-  puts "HI, I'm Java. I want to put things here:"
-  puts java.lang.System.getProperty("export")
   EXPORT_DIR = java.lang.System.getProperty("export") || "public/observations"
 else
   EXPORT_DIR = "public/observations"
