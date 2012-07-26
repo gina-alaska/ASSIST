8a36ca099150d70a9c3dca72f856f75ee2a6fe08
Scott Macfarlane <macfarlane.scott@gmail.com>
2012-07-26 13:20:46 -0800

    Initial pass at adding cruise info to the ap

---
+++ config/initializers/cruises.rb
@@ -0,0 +1,8 @@
+class Cruise
+  class << self
+    attr_accessor :id
+    attr_accessor :ship
+  end
+end
+Cruise.id = "DEVELOPMENT"
+Cruise.ship = "DEVELOPMENT"
---
+++ lib/assist.rb
@@ -0,0 +1,3 @@
+module Assist 
+  VERSION = "1.0.0"
+end
