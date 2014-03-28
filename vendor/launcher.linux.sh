2c80b7be99759ea7450b8f4acb00467f2171b485
Scott Macfarlane <scott@gina.alaska.edu>
2014-03-27 17:14:48 -0800

    Force GC for Classes, set min heap to 1G

--- vendor/launcher.linux.sh
+++ vendor/launcher.linux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Ddb=`pwd`/production.sqlite3 -Dexport=`pwd`/observations -jar ASSIST.war
+java -Ddb=`pwd`/production.sqlite3 -Dexport=`pwd`/observations -Xms1G -Xmx4G -XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -jar ASSIST.war
--- vendor/launcher.windows.bat
+++ vendor/launcher.windows.bat
@@ -4,4 +4,4 @@ set DB=%~dp0/production.sqlite3
 set EXPORT=%~dp0/observations
 set JAR=%~dp0/ASSIST.war
 
-java -Ddb="%DB%" -Dexport="%EXPORT%" -jar "%JAR%"
+java -Ddb="%DB%" -Dexport="%EXPORT%" -Xms1G -Xmx4G -XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -jar "%JAR%"
